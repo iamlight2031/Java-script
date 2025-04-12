d15d0c8bedfba74047215b4abed3378fd21c49df
iamlight2031 <anujashivani30@gmail.com>
2025-04-12 12:33:04 +0530

    condition

---
+++ condition/ifelsestmt.js
@@ -0,0 +1,47 @@
+//if else statement
+let age = 23;
+if(age>=23){
+    console.log("you are an adult");
+}
+else{
+    console.log("you are an minor");
+}
+//switch case
+let day = 5;
+switch(day){
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+        default:
+            console.log("no more days are found")
+}
+
+//for loop
+for (let i=1;i<=5;i++){
+    console.log("count:"+i);
+}
+//while loop
+let count = 1;
+while(count<5){
+    console.log("count:"+count);
+    count++;
+}
+//do while
+let number = 0;
+do{
+    console.log("number:"+number);
+    number ++;
+}
+while(number<5)
